60927c49a9101a4b5dadcb4228264685eeb5b978
Amit <theamit26@gmail.com>
2018-06-06 17:33:34 +0530

    Python Codes

--- numbers/print_armstrong.py
+++ numbers/print_armstrong.py
@@ -1,5 +1,6 @@
 print('Armstrong Number from 1 to 10000000')
-for i in range(1, 10000000):
+for i in range(1, (9**3)*7):
+    # (9**3)*7) range because Tong told about max outbound range.
     num = i
 
     total = 0
